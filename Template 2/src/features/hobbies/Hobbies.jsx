88fd6f5695164767de5b18e77badcfe13086d8b8
Curtis Martin <curtisastonmartin@hotmail.co.uk>
2023-08-22 22:51:16 +0100

    Added Hobbies Component

---
+++ Template 2/src/features/hobbies/Hobbies.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBook, faChartLine } from '@fortawesome/free-solid-svg-icons';
+
+import './Hobbies.css';
+import ContentTitle from '../content-title/ContentTitle';
+
+const HOBBIES = [
+  'Improv',
+  'Weightlifting',
+  'Archery',
+  'Reading',
+  'Motorcycling',
+  'Personal Development',
+];
+
+const Hobbies = () => {
+  library.add(faBook);
+  library.add(faChartLine);
+
+  return (
+    <div>
+      <ContentTitle icon={<FontAwesomeIcon icon="book" />} text="HOBBIES" />
+
+      <div className="hobbies-container">
+        {HOBBIES.map((i) => (
+          <div className="hobbies-text" key={i}>
+            {i}
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Hobbies;
