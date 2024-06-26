0cd2020dc217717fcad6b2b145240c40d51d10ae
Curtis Martin <curtisastonmartin@hotmail.co.uk>
2024-06-26 19:02:39 +0100

    Minor Experience Updates

--- Template 2/src/features/work-experience/WorkExperience.jsx
+++ Template 2/src/features/work-experience/WorkExperience.jsx
@@ -22,8 +22,8 @@ const WorkExperience = () => {
       >
         <div className="project-details">
           <ul>
-            <li>As part of the founding outbound sales talent, I designed, created, and oversee WANDR&apos;s outbound strategies 
-              (email, LinkedIn, and content creation) in the B2B SaaS market to create a sustainable new revenue source to 
+            <li>As part of the founding outbound sales talent, I designed, created, and oversee WANDR&apos;s outbound strategies
+              (email, LinkedIn, and content creation) in the B2B SaaS market to create a sustainable new revenue source to
               scale operations.</li>
             <li>Wrote and continue to improve (via metrics analysis) segmented multi-channel Lemlist sequences for lead outreach.</li>
             <li>Develop pipelines with Clay to find applicable leads, enrich them, and generate personalised insights and compliments using AI.</li>
@@ -42,22 +42,22 @@ const WorkExperience = () => {
         employerDetails="Yoti"
       >
         <div className="project-details">
-          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          {/* <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
             <div>
-              <b>Yoti Sign (eSignature SaaS / DocuSign competitor)</b>
+              <i>Yoti Sign (eSignature SaaS / DocuSign competitor)</i>
             </div>
-            {/* <div className="project-date">
+            <div className="project-date">
               06/2022 - 12/2023
-            </div> */}
-          </div>
+            </div>
+          </div> */}
 
           <ul>
             <li>
-              Fostered partnerships with Fortune 500 clients such as Facebook, TikTok, and Lloyds Bank to build 2-way 
-              communication lines and facilitate regular feedback sessions.
+              Fostered partnerships with Fortune 500 clients such as Facebook, TikTok, and Lloyds Bank to build 2-way
+              communication lines and facilitate regular feedback sessions for continuous improvement.
             </li>
             <li>
-              Exceeded performance KPIs by 125% for the last 3 quarters.
+              Exceeded performance KPIs (productivity output) by 125% for the last 3 quarters.
             </li>
           </ul>
         </div>
@@ -73,7 +73,7 @@ const WorkExperience = () => {
         <div className="project-details">
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
             <div>
-              <b>Cybels - Trusted Communities (R&#38;D)</b>
+              <i>Cybels - Trusted Communities (R&#38;D)</i>
             </div>
             <div className="project-date">
               03/2021 - 06/2022
@@ -82,13 +82,15 @@ const WorkExperience = () => {
 
           <ul>
             <li>
-              Developed a unique PKIaaS solution for comprehensive Air Force cyber protection.
+              Developed a unique PKIaaS (SaaS) solution for end-to-end Air Force cyber-protection to reduce terrorist attacks and sabotage.
             </li>
             <li>
-              Conducted project demonstrations for Air Force, BAE, and Boeing senior officials to secure funding.
+              Conducted project demonstrations to Air Force, BAE, and Boeing senior officials to secure further project funding, required to stop
+              the project from terminating.
             </li>
             <li>
-              Assumed role of Lead Frontend Engineer (for 4 months), providing mentorship and delegating tasks to Junior colleagues.
+              Additionally assumed the role of Lead Frontend Engineer (for 4 months), which included providing mentorship and task
+              delegation to Junior colleagues.
             </li>
           </ul>
         </div>
@@ -96,7 +98,7 @@ const WorkExperience = () => {
         <div className="project-details">
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
             <div>
-              <b>GMPS MML</b>
+              <i>GMPS MML</i>
             </div>
             <div className="project-date">
               06/2020 - 03/2021
@@ -105,14 +107,12 @@ const WorkExperience = () => {
 
           <ul>
             <li>
-              Collaborated with stakeholders to architect a solution required for the continued use of 60 active military
-              Chinook helicopters.
+              Collaborated with stakeholders to architect and develop a software solution required for the continued use of 60
+              active military Chinook helicopters, meeting all deadlines and budget constraints.
             </li>
             <li>
-              Successfully delivered project on time and within budget constraints.
-            </li>
-            <li>
-              Senior-level responsibilities (meeting runner, requirements capturing, military-grade test accreditation, etc).
+              Led meetings, gathered requirements, and managed military-grade test accreditation as part of senior-level
+              responsibilities.
             </li>
           </ul>
         </div>
@@ -120,7 +120,7 @@ const WorkExperience = () => {
         <div className="project-details">
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
             <div>
-              <b>Nuada</b>
+              <i>Nuada</i>
             </div>
             <div className="project-date">
               02/2019 - 08/2019
@@ -133,10 +133,11 @@ const WorkExperience = () => {
               military training.
             </li>
             <li>
-              Conducted customer product demonstrations and feedback sessions every month.
+              Conducted customer product demonstrations and feedback sessions every month to team members and project stakeholders.
             </li>
             <li>
-              Led end-to-end system demonstrations for senior military personnel at an annual trade show (I/ITSEC).
+              Led end-to-end system demonstrations for senior military personnel at an annual trade show (I/ITSEC)
+              leading to increased project and company visibility/reputation in the market.
             </li>
           </ul>
         </div>
@@ -144,7 +145,7 @@ const WorkExperience = () => {
         <div className="project-details">
           <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end' }}>
             <div>
-              <b>Facilities for Air Combat Training (FACT)</b>
+              <i>Facilities for Air Combat Training (FACT)</i>
             </div>
             <div className="project-date">
               01/2020 - 06/2020
@@ -155,13 +156,14 @@ const WorkExperience = () => {
 
           <ul>
             <li>
-              Developed a leading SaaS platform for Air Force training, addressing market needs overshadowed by complex legacy systems.
+              Developed a leading SaaS platform to simplify the planning and execution of military flight simulator training scenarios
+              for the Air Force, addressing market needs overshadowed by complex legacy systems.
             </li>
             <li>
-              Lead face-to-face customer product demonstrations every month.
+              Lead face-to-face customer product demonstrations every month to team members and project stakeholders.
             </li>
             <li>
-              Exceeded performance KPIs by 150% in each bi-weekly release cycle throughout the project duration.
+              Exceeded performance KPIs (productivity output) by 150% in each bi-weekly release cycle throughout the project duration.
             </li>
           </ul>
         </div>
